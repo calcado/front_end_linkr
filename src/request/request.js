a96b9e3099fdb4bd43b27f20bf89762542860c90
pedro_calcado_moreira <pecalcado@gmail.com>
2023-01-08 11:32:58 -0300

    correçao controller

--- src/request/request.js
+++ src/request/request.js
@@ -4,12 +4,12 @@ const BASE_URL = 'http://localhost:5000';
 
 export function posttrending(body) {
 
-    const promise = axios.post(`${BASE_URL}/timeline_post`, body);
+    const promise = axios.post(`${BASE_URL}/timeline`, body);
     return promise;
   }
 
   export function gettrending() {
 
-    const promise = axios.get(`${BASE_URL}/timeline_get`);
+    const promise = axios.get(`${BASE_URL}/timeline`);
     return promise;
   }
